7eb4aac0dc9e0f4bd18a6668fab6f54174ede3fb
Gabriela <161860505+GabrielaOrkoChavez@users.noreply.github.com>
2024-09-29 19:39:36 -0300

    Add files via upload

---
+++ Modal/modal.js
@@ -0,0 +1,16 @@
+function clique() {
+    let modal = document.getElementById('modal');
+    let fundoescuro = document.getElementById('fundoescuro');
+
+    fundoescuro.style.display = 'block'
+    modal.style.display = 'block'; 
+    
+}
+
+function fecharmodal(){
+    let modal = document.getElementById('modal');
+    let fundoescuro = document.getElementById('fundoescuro');
+
+    fundoescuro.style.display = 'none'
+    modal.style.display = 'none'
+}
